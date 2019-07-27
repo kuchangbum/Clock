6660f3e62cd69ad5b146ce2c52d8608f7d37ea08
ku changbum <kaze9921@gmail.com>
2019-07-27 09:53:06 +0900

    clock_vr2

--- JS Clock/clock.js
+++ JS Clock/clock.js
@@ -1,18 +1,19 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1");
 
-function getTime(){
-    const date = new Date();
-    const minnutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hour}` : hours}:${
-        minnutes < 10 ? `0${minnutes}`: minnutes
-    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
-function init(){
-    getTime();
-    setInterval(getTime, 1000);
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
